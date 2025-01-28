yaap/frameworks_base

base: Add a toggle for long press on navbar to search [1/3]

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -183,7 +183,8 @@ public class SystemSettings {
                 Settings.System.QS_BT_AUTO_ON,
                 Settings.System.BRIGHTNESS_SLIDER_HAPTICS,
                 Settings.System.VOLUME_PANEL_HAPTICS,
-                Settings.System.THREE_FINGER_GESTURE
+                Settings.System.THREE_FINGER_GESTURE,
+                Settings.System.NAVBAR_LONG_PRESS_GESTURE
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);

AlphaDroid-Project/android_frameworks_base

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -116,7 +116,8 @@ public class SystemSettings {
                 Settings.System.SCREEN_FLASH_NOTIFICATION_COLOR,
                 Settings.System.NOTIFICATION_COOLDOWN_ENABLED,
                 Settings.System.NOTIFICATION_COOLDOWN_ALL,
-                Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED
+                Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED,
+                Settings.System.NAVBAR_LONG_PRESS_GESTURE
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);